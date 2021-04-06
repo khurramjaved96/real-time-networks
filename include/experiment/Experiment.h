9fc8875f156665a4c62dbac423f36af36b46abb0
Khurramjaved <kjaved@ualberta.ca>
2021-04-05 22:29:07 -0600

    Updating experiment management files

--- include/experiment/Experiment.h
+++ include/experiment/Experiment.h
@@ -13,7 +13,7 @@
 class Experiment {
 //    std::string name;
     std::map<std::string, std::vector<std::string>> args;
-    std::map<std::string, std::string> args_for_run;
+
     std::string output_dir;
     Database d = Database();
 
@@ -21,13 +21,12 @@ class Experiment {
 //    std::map<std::string, std::string> get_args_for_run(int run, std::map<std::string, std::vector<std::string>> args, std::vector<int>);
 
 public:
+    std::map<std::string, std::string> args_for_run;
     int run;
     std::string database_name;
-//    Experiment(std::string name, std::map<std::string, std::string> args, std::string output_dir, bool sql, int rank);
     Experiment(int name, char *argv[]);
     static std::map<std::string, std::vector<std::string>> parse_params(int total_prams, char *pram_list[]);
-//    bool make_table(std::string table_name, std::vector<std::string> column_names, std::vector<std::string> data_types,  std::vector<std::string> primary_key);
-//    bool insert_values(std::string table_name, std::vector<std::string> column_names, std::vector<std::vector<std::string>> value_lists);
+    int get_int_param(const std::string&);
 };
 
 
