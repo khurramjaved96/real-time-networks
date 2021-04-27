a4a5537e4aead26997bc9264dc3c2ea5955a56d8
Khurramjaved <kjaved@ualberta.ca>
2021-04-27 17:20:27 -0600

    Test network for comparing gradients to autograd

---
+++ include/neural_networks/networks/test.h
@@ -0,0 +1,43 @@
+//
+// Created by Khurram Javed on 2021-04-25.
+//
+
+#ifndef FLEXIBLENN_TEST_H
+#define FLEXIBLENN_TEST_H
+
+
+#include "../synapse.h"
+#include <vector>
+#include "../neuron.h"
+#include <vector>
+#include <map>
+
+
+
+class TestCase{
+
+    std::vector<neuron*> output_neuros;
+    std::vector<neuron*> all_neurons;
+
+    long long int time_step;
+    std::vector<synapse*> all_synapses;
+public:
+    std::vector<float> sum_of_gradients;
+    std::vector<neuron*> input_neurons;
+    TestCase(float step_size, int width, int seed);
+    ~TestCase();
+    void print_graph(neuron* root);
+    void set_print_bool();
+
+    void set_input_values(std::vector<float> const &input_values);
+    void step();
+    std::vector<float> read_output_values();
+    std::vector<float> read_all_values();
+    float introduce_targets(std::vector<float> targets);
+    int get_input_size();
+    int get_total_synapses();
+};
+
+
+
+#endif //FLEXIBLENN_TEST_H
