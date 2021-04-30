d5ee8b8b30cf5befa48c188ac18856468806da59
Khurramjaved <kjaved@ualberta.ca>
2021-04-30 14:15:09 -0600

    Simple animal learning experiment (no distractors)

--- simple_exp.cu
+++ simple_exp.cu
@@ -22,7 +22,7 @@ int main(int argc, char *argv[]) {
 //    std::string default_config = "--name test --width 10 --seed 0 --steps 100 --run 0";
     std::cout << "Program started \n";
     float gamma = 1 - 1.0/(3.0);
-    TraceConditioning tc = TraceConditioning(std::pair<int, int>(20, 20), std::pair<int, int>(60, 100), 2, 2);
+    TraceConditioning tc = TraceConditioning(std::pair<int, int>(4, 4), std::pair<int, int>(15, 15), std::pair<int, int>(60, 100), 5, 2);
     for(int temp = 0; temp<200; temp++)
     {
         std::vector<float> cur_state = tc.step();
@@ -34,86 +34,137 @@ int main(int argc, char *argv[]) {
     std::cout << "Experiment object created \n";
     int width = my_experiment.get_int_param("width");
 
-    Metric synapses_metric = Metric(my_experiment.database_name, "error_table", std::vector<std::string>{"step", "run", "error"}, std::vector<std::string>{"int", "int", "real"}, std::vector<std::string>{"step", "run"});
+    Metric synapses_metric = Metric(my_experiment.database_name, "error_table", std::vector<std::string>{"step", "run", "error", "error_type"}, std::vector<std::string>{"int", "int", "real", "int"}, std::vector<std::string>{"step", "run", "error_type"});
 //    std::cout << "Database stuff done \n";
     CustomNetwork my_network = CustomNetwork(my_experiment.get_float_param("step_size"),
                                              my_experiment.get_int_param("width"), my_experiment.get_int_param("seed"));
 //    my_network.set_print_bool();
-    long long int time_step = 0;
+
 //    for(auto &it : my_network.input_neurons)
 //        my_network.print_graph(it);
     std::cout << "Total synapses in the network " << my_network.get_total_synapses() << std::endl;
     auto start = std::chrono::steady_clock::now();
-    float running_error = -1;
+    std::vector<float> running_error;
+    running_error.push_back(-1);
+    running_error.push_back(-1);
     std::vector<std::vector<std::string>> error_logger;
+    float prediction = 0;
+
+    float prediction_long = 0;
+
+    float real_target = 0;
+    float real_target_long = 0;
+    float R = 0;
+    float  old_R = 0;
+    float R_long = 0;
+    float old_R_long = 0;
     for (int counter = 0; counter < my_experiment.get_int_param("steps"); counter++) {
 
+
+
         std::vector<std::string> error;
-
-
         std::vector<float> temp_target;
 
-        temp_target.push_back(tc.get_target(gamma));
-        my_network.set_input_values(tc.step());
-
-//        temp_target.push_back(0);
-//        if (counter % 3 == 0) {
-//            my_network.set_input_values(std::vector<float>{1, 1});
-//            temp_target.push_back(0.50);
-//            temp_target.push_back(-0.50);
-//        } else if (counter % 3 == 1) {
-//            my_network.set_input_values(std::vector<float>{1, 0});
-//            temp_target.push_back(-0.1);
-//            temp_target.push_back(-0.3);
-//        } else {
-//            my_network.set_input_values(std::vector<float>{0, 1});
-//            temp_target.push_back(-0.50);
-//            temp_target.push_back(0.40);
-//        }
-
-        if (running_error == -1)
-            running_error = my_network.introduce_targets(temp_target);
-        else
-            running_error = running_error * 0.999 + 0.001 * my_network.introduce_targets(temp_target);
+        auto state_current = tc.step();
+        std::vector<float> new_vec;
+        for(auto & it: state_current){
+            new_vec.push_back(it);
+        }
+//        new_vec[1] = 0;
+//        new_vec[2] = 0;
+        my_network.set_input_values(state_current);
 
 
+        old_R = R;
+        R = tc.get_US();
 
-        long long int time_step = 0;
-
-
-//
+        old_R_long = R_long;
+        R_long = tc.get_long_US();
         my_network.step();
 
-        if(counter % 500 == 0)
+        prediction = my_network.read_output_values()[0];
+
+        prediction_long = my_network.read_output_values()[1];
+
+        real_target = tc.get_target(gamma);
+        real_target_long = tc.get_target_long(gamma);
+        float target = prediction * gamma + old_R;
+        float target_long = prediction_long * gamma + old_R_long;
+        if(counter > 0) {
+            float error_short = (my_network.read_output_values()[0] - real_target) *
+                          (my_network.read_output_values()[0] - real_target);
+            float error_long = (my_network.read_output_values()[1] - real_target_long) *
+                               (my_network.read_output_values()[1] - real_target_long);
+            temp_target.push_back(target);
+            temp_target.push_back(target_long);
+            my_network.introduce_targets(temp_target);
+            if (running_error[0] == -1) {
+                running_error[0] = error_short;
+                running_error[1] = error_long;
+            }
+
+            else {
+                running_error[0] = running_error[0] * 0.999 + 0.001 *error_short;
+                running_error[1] = running_error[1] * 0.999 + 0.001 *error_long;
+            }
+        }
+        if(counter % 300 == 0)
         {
             error.push_back(std::to_string(counter));
             error.push_back(std::to_string(my_experiment.get_int_param("run")));
+            error.push_back(std::to_string(running_error[0]));
+            error.push_back(std::to_string(0));
 
-            error.push_back(std::to_string(running_error));
+            error_logger.push_back(error);
+            error.clear();
+            error.push_back(std::to_string(counter));
+            error.push_back(std::to_string(my_experiment.get_int_param("run")));
+            error.push_back(std::to_string(running_error[1]));
+            error.push_back(std::to_string(1));
+
             error_logger.push_back(error);
 
         }
-        if(counter % 10000 < 100)
+        if(counter % 10000 < 500)
         {
             std::vector<float> cur_state = tc.get_state();
-            cur_state.push_back(tc.get_target(gamma) );
+            cur_state.push_back(real_target);
             cur_state.push_back(my_network.read_output_values()[0]);
+            cur_state.push_back(target);
+            cur_state.push_back(real_target_long);
+            cur_state.push_back(my_network.read_output_values()[1]);
+            cur_state.push_back(target_long);
 //            cur_state[2] = tc.get_target(gamma);
 //            cur_state[3] = my_network.read_output_values()[0];
             print_vector(cur_state);
         }
-        if(counter % 10000 == 0)
+        if(counter%500000  == 499999)
         {
+            my_network.add_memory(my_experiment.get_float_param("step_size"));
+            my_network.add_memory(my_experiment.get_float_param("step_size"));
+            my_network.add_memory(my_experiment.get_float_param("step_size"));
+
+
+        }
+//
+        if(counter % 10000 == 9998)
+        {
+            print_vector(my_network.get_memory_weights());
+            std::cout << "Pushing results" << std::endl;
             synapses_metric.add_values(error_logger);
+            std::cout << "Results added " << std::endl;
+            std::cout << "Len = " << error_logger.size() << std::endl;
+//            exit(1);
             error_logger.clear();
         }
-        if (counter % 1000 == 0 || counter % 10000 == 999 || counter % 10000 == 998) {
+        if (counter % 10000 == 0 || counter % 10000 == 999 || counter % 10000 == 998) {
             std::cout << "### STEP = " << counter << std::endl;
-            std::cout << "Running error = " << running_error << std::endl;
+            std::cout << "Running error = ";
+            print_vector(running_error);
+//            my_network.print_memory_feature_weights();
 
-
-            print_vector(my_network.read_output_values());
-            std::cout << "Target = " << tc.get_target(gamma) << std::endl;
+//            print_vector(my_network.read_output_values());
+//            std::cout << "Target = " << tc.get_target(gamma) << std::endl;
         }
     }
 
