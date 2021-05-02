73d6c4f9cd93303588667c7fb8e64fa7dddb9071
Khurramjaved <kjaved@ualberta.ca>
2021-05-02 11:02:31 -0600

    Adding getters for getting network graph

--- src/utils.cpp
+++ src/utils.cpp
@@ -77,6 +77,50 @@ void NetworkVisualizer::generate_dot(int time_step) {
     dot_file.close();
 }
 
+std::string NetworkVisualizer::get_graph(int time_step) {
+    dot_string = "digraph network{\n"
+                 "\tnode [shape = circle];\n";
+
+    for(auto &it : all_neurons) {
+        for (auto &os: it->outgoing_synapses) {
+            auto current_n = os;
+            dot_string += "\t" + std::to_string(current_n->input_neurons->id)
+                          + "->" + std::to_string(current_n->output_neurons->id) //+ ";\n";
+                          + "[label = \"" + std::to_string(os->weight) + "\"];\n";
+        }
+    }
+    dot_string += "\n}";
+    return dot_string;
+}
+
+std::string NetworkVisualizer::get_graph_detailed(int time_step) {
+    dot_string = "digraph network{\n"
+                 "\tconcentrate=True;\n"
+                 "\trankdir=TB;\n"
+                 "\tnode [shape=record];";
+
+    for(auto &it : all_neurons) {
+        dot_string += "\t" + std::to_string(it->id)
+                      + " [label=\"" + std::to_string(it->id) + "\\n|"
+                      + "{past_act_size:|err_grad_size:|value:}"
+                      + "|{" + std::to_string(it->past_activations.size()) +"|"
+                      +  std::to_string(it->error_gradient.size()) + "|"
+                      +  std::to_string(it->value) +"}"
+                      + "\"];\n";
+        for (auto &os: it->outgoing_synapses) {
+            auto current_n = os;
+            dot_string += "\t" + std::to_string(current_n->input_neurons->id)
+                          + "->" + std::to_string(current_n->output_neurons->id) + ";\n";
+            //+ " [label = \"gq_size:" + std::to_string(os->grad_queue.size())
+            //+ " credit:" + std::to_string(os->credit) + "\"];\n";
+
+            // + "[label = \"" + std::to_string(os->weight) + "\"];\n";
+        }
+    }
+    dot_string += "\n}";
+    return dot_string;
+}
+
 
 void NetworkVisualizer::generate_dot_detailed(int time_step) {
     dot_string = "digraph network{\n"
