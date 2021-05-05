00fe9b333754b2caa6303a589eac1779980f5da4
Haseeb Shah <haseebs@outlook.com>
2021-05-04 17:58:05 -0700

    add accuracy, more bool params, more logging

--- run_copy_task.cu
+++ run_copy_task.cu
@@ -20,65 +20,93 @@
 
 
 int main(int argc, char *argv[]) {
-//    std::string default_config = "--name test --width 10 --seed 0 --steps 100 --run 0 --step_size 0.0001 --num_layers 5";
+//    std::string default_config = "--name test --width 10 --seed 0 --steps 100 --run 0 --step_size 0.0001 --num_layers 5 --data_driven_initialization false --randomize_sequence_length true --add_features false"
 //    __builtin_trap();
     std::cout << "Program started \n";
-    Experiment my_experiment = Experiment(argc, argv);
+    Experiment exp = Experiment(argc, argv);
     std::cout << "Experiment object created \n";
-    int width = my_experiment.get_int_param("width");
+    int width = exp.get_int_param("width");
 
-    Metric synapses_metric = Metric(my_experiment.database_name, "error_table", std::vector<std::string>{"step", "datatime", "seq_length", "run", "error"}, std::vector<std::string>{"int", "int", "int", "int", "real"}, std::vector<std::string>{"step",  "run" });
-    Metric observations_metric = Metric(my_experiment.database_name, "obs_table", std::vector<std::string>{"run", "step", "pred_flag", "inp_seq", "target", "pred", "L", "seq_len", "data_timestep"}, std::vector<std::string>{"int", "int", "real", "real", "real", "real", "int", "int", "int"}, std::vector<std::string>{"run", "step"});
-    Metric graph_state = Metric(my_experiment.database_name, "graph", std::vector<std::string>{"step", "run", "graph_data"}, std::vector<std::string>{"int", "int", "MEDIUMTEXT"}, std::vector<std::string>{"step", "run"});
-    CustomNetwork my_network = CustomNetwork(my_experiment.get_float_param("step_size"),
-                                             my_experiment.get_int_param("width"),
-                                             my_experiment.get_int_param("num_layers"),
-                                             my_experiment.get_int_param("seed"));
+    Metric synapses_metric = Metric(exp.database_name, "error_table",
+                                    std::vector<std::string>{"step", "datatime", "seq_length", "run", "error", "accuracy"},
+                                    std::vector<std::string>{"int", "int", "int", "int", "real", "real"},
+                                    std::vector<std::string>{"step",  "run" });
+    Metric run_state_metric = Metric(exp.database_name, "state_table",
+                                     std::vector<std::string>{"run", "state", "state_comments"},
+                                     std::vector<std::string>{"int", "MEDIUMTEXT", "MEDIUMTEXT"},
+                                     std::vector<std::string>{"run"});
+    Metric observations_metric = Metric(exp.database_name, "obs_table",
+                                        std::vector<std::string>{"run", "step", "pred_flag", "inp_seq", "target", "pred", "L", "seq_len", "data_timestep"},
+                                        std::vector<std::string>{"int", "int", "real", "real", "real", "real", "int", "int", "int"},
+                                        std::vector<std::string>{"run", "step"});
+    Metric graph_state = Metric(exp.database_name, "graph",
+                                std::vector<std::string>{"step", "run", "graph_data"},
+                                std::vector<std::string>{"int", "int", "MEDIUMTEXT"},
+                                std::vector<std::string>{"step", "run"});
 
-    CopyTask env = CopyTask(my_experiment.get_int_param("seed"));
+    CustomNetwork my_network = CustomNetwork(exp.get_float_param("step_size"),
+                                             exp.get_int_param("width"),
+                                             exp.get_int_param("num_layers"),
+                                             exp.get_int_param("seed"));
+
+    CopyTask env = CopyTask(exp.get_int_param("seed"), exp.get_bool_param("randomize_sequence_length"));
     //get a sequence of data for data-driven initialization
-    std::vector<std::vector<float>> input_batch;
-//    input_batch.reserve(500);
-//    for(int temp=0; temp<500; temp++)
-//        input_batch.push_back(env.step(1));
-//    my_network.initialize_network(input_batch);
-    env.reset();
+    if (exp.get_bool_param("data_driven_initialization")){
+        std::vector<std::vector<float>> input_batch;
+        input_batch.reserve(500);
+        for(int temp=0; temp<500; temp++)
+            input_batch.push_back(env.step(1));
+        my_network.initialize_network(input_batch);
+        env.reset();
+    }
 
     std::cout << "Total synapses in the network " << my_network.get_total_synapses() << std::endl;
-//    my_network.viz_graph();
     auto start = std::chrono::steady_clock::now();
 
     float running_error = -1;
+    float running_accuracy = -1;
     float target = 0;
     float prediction = 0;
     float last_err = 1;
     float target_old = 0;
     int current_seq_length = 1;
+    std::string state = "finished";
+    std::string state_comments = "";
     std::vector<std::vector<std::string>> error_logger;
     std::vector<std::vector<std::string>> state_logger;
+    std::vector<std::vector<std::string>> graph_logger;
 
     std::cout << "Flag Bit \t Pred Bit \t Target \t Pred \t Seq_len \t Datatime" << std::endl;
-    for (int counter = 0; counter < my_experiment.get_int_param("steps"); counter++) {
+    for (int counter = 0; counter < exp.get_int_param("steps"); counter++) {
 
         auto state_current = env.step(last_err);
 //        print_vector(state_current);
         my_network.set_input_values(state_current);
         my_network.step();
         prediction = my_network.read_output_values()[0];
-        // just realized that this sigmoid doesnt make any sense. It is just hiding the problem since it is not involved in the feedback.
-//        prediction = sigmoid(my_network.read_output_values()[0]);
+        if(isnan(prediction)){
+          state = "killed";
+          state_comments = "nan_prediction";
+          break;
+        }
+
         target_old = target;
         target = env.get_target();
         if(counter > 0)
             my_network.introduce_targets(std::vector<float>{target_old});
 
-//        float error = (prediction - target) * (prediction - target);
         float error = (prediction - target)*(prediction - target);
         last_err = error;
-        if (running_error == -1)
+        float accuracy;
+        (prediction > 0.5 && target == 1) || (prediction < 0.5 && target == 0) ? accuracy=1 : accuracy=0;
+        if (running_error == -1){
             running_error = error;
-        else
+            running_accuracy = accuracy;
+        }
+        else{
             running_error = running_error * 0.999 + 0.001 *error;
+            running_accuracy = running_accuracy * 0.999 + 0.001 *accuracy;
+        }
 
 
 
@@ -88,8 +116,9 @@ int main(int argc, char *argv[]) {
             error_vec.push_back(std::to_string(counter));
             error_vec.push_back(std::to_string(env.get_data_timestep()));
             error_vec.push_back(std::to_string(env.get_L()));
-            error_vec.push_back(std::to_string(my_experiment.get_int_param("run")));
+            error_vec.push_back(std::to_string(exp.get_int_param("run")));
             error_vec.push_back(std::to_string(running_error));
+            error_vec.push_back(std::to_string(running_accuracy));
             error_logger.push_back(error_vec);
             current_seq_length = env.get_L();
         }
@@ -97,10 +126,10 @@ int main(int argc, char *argv[]) {
 
         std::vector<float> cur_state = env.get_state();
 
-        if(counter < 5000)
+        if(counter < 1000 || counter > exp.get_int_param("steps") - 1000)
         {
             std::vector<std::string> state_vec;
-            state_vec.push_back(std::to_string(my_experiment.get_int_param("run")));
+            state_vec.push_back(std::to_string(exp.get_int_param("run")));
             state_vec.push_back(std::to_string(counter));
             state_vec.push_back(std::to_string(cur_state[0]));
             state_vec.push_back(std::to_string(cur_state[1]));
@@ -110,6 +139,15 @@ int main(int argc, char *argv[]) {
             state_vec.push_back(std::to_string(env.get_seq_length()));
             state_vec.push_back(std::to_string(env.get_data_timestep()));
             state_logger.push_back(state_vec);
+        }
+
+        if(counter < 10){
+            std::string g = my_network.get_viz_graph();
+            std::vector<std::string> graph_data;
+            graph_data.push_back(std::to_string(exp.get_int_param("steps")));
+            graph_data.push_back(std::to_string(exp.get_int_param("run")));
+            graph_data.push_back(g);
+            graph_logger.push_back(graph_data);
         }
 
         if(counter % 50000 < 200)
@@ -122,16 +160,21 @@ int main(int argc, char *argv[]) {
             print_vector(cur_state);
         }
 
-//        if(counter%500000  == 499999)
-//        {
-//            my_network.add_memory(my_experiment.get_float_param("step_size"));
-//            my_network.add_memory(my_experiment.get_float_param("step_size"));
-//            my_network.add_memory(my_experiment.get_float_param("step_size"));
-//            my_network.add_memory(my_experiment.get_float_param("step_size"));
-//            my_network.add_memory(my_experiment.get_float_param("step_size"));
-//
-//
-//        }
+        if(counter%500000  == 499999 && exp.get_bool_param("add_features"))
+        {
+            my_network.add_memory(exp.get_float_param("step_size"));
+            my_network.add_memory(exp.get_float_param("step_size"));
+            my_network.add_memory(exp.get_float_param("step_size"));
+            my_network.add_memory(exp.get_float_param("step_size"));
+            my_network.add_memory(exp.get_float_param("step_size"));
+
+            std::string g = my_network.get_viz_graph();
+            std::vector<std::string> graph_data;
+            graph_data.push_back(std::to_string(exp.get_int_param("steps")));
+            graph_data.push_back(std::to_string(exp.get_int_param("run")));
+            graph_data.push_back(g);
+            graph_logger.push_back(graph_data);
+        }
 
         if(counter % 100000 == 99998)
         {
@@ -153,16 +196,23 @@ int main(int argc, char *argv[]) {
     auto end = std::chrono::steady_clock::now();
     std::cout << "Elapsed time in milliseconds for per steps: "
               << 1000000 / (std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() /
-                            my_experiment.get_int_param("steps"))
+                            exp.get_int_param("steps"))
               << " fps" << std::endl;
 
 
     std::string g = my_network.get_viz_graph();
     std::vector<std::string> graph_data;
-    graph_data.push_back(std::to_string(my_experiment.get_int_param("steps")));
-    graph_data.push_back(std::to_string(my_experiment.get_int_param("run")));
+    graph_data.push_back(std::to_string(exp.get_int_param("steps")));
+    graph_data.push_back(std::to_string(exp.get_int_param("run")));
     graph_data.push_back(g);
-    graph_state.add_value(graph_data);
+    graph_logger.push_back(graph_data);
+    graph_state.add_values(graph_logger);
+
+    std::vector<std::string> state_data;
+    state_data.push_back(std::to_string(exp.get_int_param("run")));
+    state_data.push_back(state);
+    state_data.push_back(state_comments);
+    run_state_metric.add_value(state_data);
 
     return 0;
 }
