6eeffb524f25ddb62106256a163d2241a7ce52cb
Khurramjaved <kjaved@ualberta.ca>
2021-07-19 10:07:59 -0600

    Refactor

--- include/neural_networks/neuron.h
+++ include/neural_networks/neuron.h
@@ -6,7 +6,6 @@
 #define BENCHMARKS_NEURON_H
 
 
-
 #include <vector>
 #include <queue>
 #include <mutex>
@@ -16,47 +15,87 @@
 #include "utils.h"
 #include <utility>
 #include "dynamic_elem.h"
-//class synapse;
 
 
-class neuron : public dynamic_elem  {
-    bool cycle_check;
-
+class neuron : public dynamic_elem {
 public:
-    static int neuron_id;
+    static long long int neuron_id_generator;
+    synapse* recurrent_synapse;
+    float old_value;
+    bool is_recurrent_neuron;
     bool is_input_neuron;
-    static normal_random normal_dist;
     float value;
-    float temp_value;
+    int drinking_age;
+    float shadow_error_prediction_before_firing;
+    float shadow_error_prediction;
+    float value_before_firing;
     int memory_made;
-    bool activation_type;
+    float neuron_utility;
+    bool is_relu;
     bool is_output_neuron;
     bool useless_neuron;
     int sucesses;
     int failures;
-    int id;
-    bool mature;
+    long long int id;
+    bool is_mature;
     int neuron_age;
     float average_activation;
+
     void forward_gradients();
+
     void update_value();
+
     std::queue<message> error_gradient;
-    std::queue<std::pair<float, int>> past_activations;
-    std::vector<synapse*> outgoing_synapses;
-    std::vector<synapse*> incoming_synapses;
-//    std::vector<std::reference_wrapper<neuron>> output_nodes;
+    std::queue<message_activation> past_activations;
+    std::vector<synapse *> outgoing_synapses;
+    std::vector<synapse *> incoming_synapses;
+
     neuron(bool activation);
+
     neuron(bool activation, bool output_n);
+
     neuron(bool activation, bool output_n, int id);
-    neuron(bool activation, bool output_n, bool input_n) ;
+
+    neuron(bool activation, bool output_n, bool input_n);
+
     void fire(int time_step);
+
     float introduce_targets(float target, int timestep);
+
     float introduce_targets(float target, int timestep, float gamma, float lambda);
+
     void propagate_error();
+
+    void update_utility();
+
     void mark_useless_weights();
+
     void prune_useless_weights();
 
-    ~neuron()=default;
+    ~neuron() = default;
 };
+
+
+//class recurrent_neuron : public neuron {
+//public:
+//
+//
+//    synapse *recurrent_synapse;
+//
+//    recurrent_neuron(bool activation, bool output_n, bool input_n);
+//
+////    void fire(int time_step);
+//
+//    float introduce_targets(float target, int timestep, float gamma, float lambda);
+//
+//    void propagate_error();
+//
+//    void mark_useless_weights();
+//
+//    void prune_useless_weights();
+//
+//    ~recurrent_neuron() = default;
+//
+//};
 
 #endif //BENCHMARKS_NEURON_H
