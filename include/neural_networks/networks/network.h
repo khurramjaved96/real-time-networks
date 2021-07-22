ec5a72bb438bed3fca14cee878132dadc9917e9f
Khurramjaved <kjaved@ualberta.ca>
2021-07-22 11:39:44 -0600

    Add base network class

---
+++ include/neural_networks/networks/network.h
@@ -0,0 +1,59 @@
+//
+// Created by Khurram Javed on 2021-07-21.
+//
+
+#ifndef INCLUDE_NEURAL_NETWORKS_NETWORKS_NETWORK_H
+#define INCLUDE_NEURAL_NETWORKS_NETWORKS_NETWORK_H
+
+#include <vector>
+#include <map>
+#include <random>
+#include <string>
+#include "../synapse.h"
+#include "../neuron.h"
+#include "../dynamic_elem.h"
+
+class Network {
+ protected:
+    int64_t time_step;
+    std::mt19937 mt;
+
+ public:
+    std::vector<neuron *> output_neurons;
+    std::vector<synapse *> all_synapses;
+    std::vector<synapse *> output_synapses;
+    std::vector<dynamic_elem *> all_heap_elements;
+    std::vector<neuron *> all_neurons;
+    std::vector<neuron *> input_neurons;
+
+    void collect_garbage();
+
+    Network();
+
+    ~Network();
+
+
+    int64_t get_timestep();
+
+    void set_input_values(std::vector<float> const &input_values);
+
+    void step();
+
+    std::vector<float> read_output_values();
+
+    std::vector<float> read_all_values();
+
+    float introduce_targets(std::vector<float> targets);
+
+    float introduce_targets(std::vector<float> targets, float gamma, float lambda);
+
+    int get_input_size();
+
+    int get_total_synapses();
+
+    int get_total_neurons();
+
+//    virtual void add_feature() = 0;
+};
+
+#endif  // INCLUDE_NEURAL_NETWORKS_NETWORKS_NETWORK_H
---
+++ src/neural_network/networks/network.cpp
@@ -0,0 +1,256 @@
+#include "../../../include/neural_networks/networks/network.h"
+
+#include <assert.h>
+#include <cmath>
+#include <random>
+#include <execution>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include "../../../include/neural_networks/neuron.h"
+#include "../../../include/neural_networks/synapse.h"
+#include "../../../include/neural_networks/dynamic_elem.h"
+#include "../../../include/utils.h"
+#include "../../../include/neural_networks/utils.h"
+
+/**
+ * Continually adapting neural network.
+ * Essentially a neural network with the ability to add and remove neurons
+ * based on a generate and test approach.
+ * Check the corresponding header file for a description of the variables.
+ *
+ * As a quick note as to how this NN works - it essentially fires all neurons once
+ * per step, unlike a usual NN that does a full forward pass per output needed.
+ *
+ * @param step_size: neural network step size.
+ * @param width: [NOT CURRENTLY USED] neural network width
+ * @param seed: random seed to initialize.
+ */
+
+int Network::get_total_neurons() {
+    int tot = 0;
+    for (auto it : this->all_neurons) {
+        if (it->is_mature)
+            tot++;
+    }
+    return tot;
+}
+
+Network::Network() {
+}
+
+
+int64_t Network::get_timestep() {
+    return this->time_step;
+}
+
+
+int Network::get_input_size() {
+    return this->input_neurons.size();
+}
+
+int Network::get_total_synapses() {
+    int tot = 0;
+    for (auto it : this->all_synapses) {
+        if (it->output_neuron->is_mature)
+            tot++;
+    }
+    return tot;
+}
+
+Network::~Network() {
+    for (auto &it : this->all_heap_elements)
+        delete it;
+}
+
+
+void Network::set_input_values(std::vector<float> const &input_values) {
+//    assert(input_values.size() == this->input_neurons.size());
+    for (int i = 0; i < input_values.size(); i++) {
+        if (i < this->input_neurons.size()) {
+            this->input_neurons[i]->value_before_firing = input_values[i];
+        } else {
+            std::cout << "More input features than input neurons\n";
+            exit(1);
+        }
+    }
+}
+
+
+/**
+ * Step function after putting in the inputs to the neural network.
+ * This function takes a step in the NN by firing all neurons.
+ * Afterwards, it calculates gradients based on previous error and
+ * propagates it back. Currently backprop is truncated at 1 step.
+ * Finally, it updates its weights and prunes is_useless neurons and synapses.
+ */
+void Network::step() {
+    std::for_each(
+            std::execution::par_unseq,
+            all_neurons.begin(),
+            all_neurons.end(),
+            [&](neuron *n) {
+                n->fire(this->time_step);
+            });
+
+//  Calculate and temporarily hold our next neuron values.
+    std::for_each(
+            std::execution::par_unseq,
+            all_neurons.begin(),
+            all_neurons.end(),
+            [&](neuron *n) {
+                n->update_value();
+            });
+
+//  Contrary to the name, this function passes gradients BACK to the incoming synapses
+//  of each neuron.
+    std::for_each(
+            std::execution::par_unseq,
+            all_neurons.begin(),
+            all_neurons.end(),
+            [&](neuron *n) {
+                n->forward_gradients();
+            });
+
+//  Now we propagate our error backwards one step
+    std::for_each(
+            std::execution::par_unseq,
+            all_neurons.begin(),
+            all_neurons.end(),
+            [&](neuron *n) {
+                n->propagate_error();
+            });
+
+//  Calculate our credit
+    std::for_each(
+            std::execution::par_unseq,
+            all_synapses.begin(),
+            all_synapses.end(),
+            [&](synapse *s) {
+                s->assign_credit();
+            });
+
+//  Update our weights (based on either normal update or IDBD update
+    std::for_each(
+            std::execution::par_unseq,
+            all_synapses.begin(),
+            all_synapses.end(),
+            [&](synapse *s) {
+                s->update_weight();
+            });
+
+//  Mark all is_useless weights and neurons for deletion
+    std::for_each(
+            std::execution::par_unseq,
+            all_neurons.begin(),
+            all_neurons.end(),
+            [&](neuron *n) {
+                n->mark_useless_weights();
+            });
+
+//  Delete our is_useless weights and neurons
+    std::for_each(
+            all_neurons.begin(),
+            all_neurons.end(),
+            [&](neuron *n) {
+                n->prune_useless_weights();
+            });
+
+//  For all synapses, if the synapse is is_useless set it has 0 references. We remove it.
+
+    std::for_each(
+            std::execution::par_unseq,
+            this->all_synapses.begin(),
+            this->all_synapses.end(),
+            [&](synapse *s) {
+                if (s->is_useless) {
+                    s->decrement_reference();
+                }
+            });
+    auto it = std::remove_if(this->all_synapses.begin(), this->all_synapses.end(), to_delete_s);
+    this->all_synapses.erase(it, this->all_synapses.end());
+
+//  Similarly for all outgoing synapses and neurons.
+    std::for_each(
+            std::execution::par_unseq,
+            this->output_synapses.begin(),
+            this->output_synapses.end(),
+            [&](synapse *s) {
+                if (s->is_useless) {
+                    s->decrement_reference();
+                }
+            });
+    it = std::remove_if(this->output_synapses.begin(), this->output_synapses.end(), to_delete_s);
+    this->output_synapses.erase(it, this->output_synapses.end());
+
+
+    std::for_each(
+            std::execution::par_unseq,
+            this->all_neurons.begin(),
+            this->all_neurons.end(),
+            [&](neuron *s) {
+                if (s->useless_neuron) {
+                    s->decrement_reference();
+                }
+            });
+
+    auto it_n = std::remove_if(this->all_neurons.begin(), this->all_neurons.end(), to_delete_n);
+    this->all_neurons.erase(it_n, this->all_neurons.end());
+//    }
+
+
+    this->time_step++;
+}
+
+
+/**
+ * Find all synapses and neurons with 0 references to them and delete them.
+ */
+void Network::collect_garbage() {
+    for (int temp = 0; temp < this->all_heap_elements.size(); temp++) {
+        if (all_heap_elements[temp]->references == 0) {
+            delete all_heap_elements[temp];
+            all_heap_elements[temp] = nullptr;
+        }
+    }
+
+    auto it = std::remove_if(this->all_heap_elements.begin(), this->all_heap_elements.end(), is_null_ptr);
+    this->all_heap_elements.erase(it, this->all_heap_elements.end());
+}
+
+std::vector<float> Network::read_output_values() {
+    std::vector<float> output_vec;
+    output_vec.reserve(this->output_neurons.size());
+    for (auto &output_neuro : this->output_neurons) {
+        output_vec.push_back(output_neuro->value);
+    }
+    return output_vec;
+}
+
+std::vector<float> Network::read_all_values() {
+    std::vector<float> output_vec;
+    output_vec.reserve(this->all_neurons.size());
+    for (auto &output_neuro : this->all_neurons) {
+        output_vec.push_back(output_neuro->value);
+    }
+    return output_vec;
+}
+
+
+float Network::introduce_targets(std::vector<float> targets) {
+    float error = 0;
+    for (int counter = 0; counter < targets.size(); counter++) {
+        error += this->output_neurons[counter]->introduce_targets(targets[counter], this->time_step - 1);
+    }
+    return error;
+}
+
+float Network::introduce_targets(std::vector<float> targets, float gamma, float lambda) {
+//  Put all targets into our neurons.
+    float error = 0;
+    for (int counter = 0; counter < targets.size(); counter++) {
+        error += this->output_neurons[counter]->introduce_targets(targets[counter], this->time_step - 1, gamma, lambda);
+    }
+    return error * error;
+}
