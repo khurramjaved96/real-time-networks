2f41450ddc879c0f010657c53a5652e7c999fce2
Haseeb Shah <haseebs@outlook.com>
2021-09-01 17:03:37 -0700

    eval for imprinting prediction

--- python_scripts/agents/sarsa_prediction_agent.py
+++ python_scripts/agents/sarsa_prediction_agent.py
@@ -1,7 +1,12 @@
+import sys
 import random
 import numpy as np
 
 from .base_agent import BaseAgent
+#sys.path.append("../utils")
+#from utils import compute_return_error
+from ..utils.utils import compute_return_error
+
 
 class SarsaPredictionAgent(BaseAgent):
     """Implements the sarsa control agent
@@ -19,16 +24,17 @@ class SarsaPredictionAgent(BaseAgent):
                 -
         """
         done = True
-        running_eps_reward = -2000
         #TODO running error
         eps_count = 0
-        eps_rewards = 0
+        eps_rewards = []
+        eps_predictions = []
+        running_MSRE = -1
         obs = env.reset()
         for t in range(timesteps):
 
             model.set_input_values(obs)
             model.step()
-            qvalues = model.read_output_values()
+            prediction = model.read_output_values()
 
             if done:  # new episode starts
                 action = self.expert_agent.predict(obs)
@@ -38,24 +44,33 @@ class SarsaPredictionAgent(BaseAgent):
             if done:
                 new_target = reward
             else:
-                next_qvalues = model.forward_pass_without_side_effects(next_obs)
-                next_action = self.expert_agent.predict(next_obs)
-                new_target = reward + gamma * next_qvalues[0]
+                bootstrap_prediction = model.forward_pass_without_side_effects(next_obs)
+                bootstrap_action = self.expert_agent.predict(next_obs)
+                new_target = reward + gamma * bootstrap_prediction[0]
 
             err = model.introduce_targets([new_target], gamma, lmbda)
-            print(err)
 
             obs = next_obs
-            action = next_action
-            eps_rewards += reward
+            action = bootstrap_action
+            eps_rewards.append(reward)
+            eps_predictions.append(prediction)
 
             if done:
                 obs = env.reset()
-                running_eps_reward = 0.01 * eps_rewards + 0.99 * running_eps_reward
-                #print(env.expanding_size)
-                print(t, eps_count, eps_rewards, running_eps_reward, qvalues)
-                eps_rewards = 0
+                MSRE, return_error, return_target = compute_return_error(eps_rewards, eps_predictions, gamma)
+                if eps_count == 0:
+                    running_MSRE = MSRE
+                else:
+                    running_MSRE = 0.999 * running_MSRE + 0.001 * MSRE
+                print(t, eps_count, MSRE, running_MSRE, prediction)
+                #if (eps_count == 1000):
+                #    from IPython import embed; embed()
+                #    exit()
+
                 eps_count += 1
+                eps_rewards = []
+                eps_predictions = []
+                #TODO reset network state
                 model.reset_trace()
         env.close()
 
---
+++ python_scripts/utils/utils.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def compute_return_error(cumulants, predictions, gamma):
+    num_time_steps = len(cumulants)
+    returns = np.zeros(num_time_steps)
+    returns[-1] = cumulants[-1]
+    for t in range(num_time_steps - 2, -1, -1):
+        returns[t] = gamma * returns[t + 1] + cumulants[t]
+    return_error = (predictions - returns) ** 2
+    MSRE = return_error.mean()
+    return MSRE, return_error, returns
+
+
+def plot_last_n(rewards_vec, predictions_vec, return_target, return_error, n=1000):
+    fig, axs = plt.subplots(4,figsize=(30, 35))
+    fig.tight_layout()
+    axs[0].step(list(range(n)), rewards_vec[-n:])
+    axs[1].step(list(range(n)), predictions_vec[-n:])
+    axs[2].step(list(range(n)), return_target[-n:])
+    axs[3].step(list(range(n)), return_error[-n:])
+
+    axs[0].title.set_text('reward')
+    axs[1].title.set_text('predictions')
+    axs[2].title.set_text('true target')
+    axs[3].title.set_text('error (prediction : true target)')
+    for ax in axs:
+        ax.grid(color='#666666', linestyle='-', alpha=0.5)
+    return fig
+
