6e0c5ef695ef97861af024e9607057a2dea22b4d
Haseeb Shah <haseebs@outlook.com>
2021-09-08 14:20:45 -0600

    Fix pruning default to what it was before your merge

--- src/nn/neuron.cpp
+++ src/nn/neuron.cpp
@@ -28,7 +28,7 @@ Neuron::Neuron(bool is_input, bool is_output) {
   references = 0;
   neuron_utility = 0;
   drinking_age = 5000;
-  mark_useless_prob = 0.99;
+  mark_useless_prob = 0.9999;
   is_bias_unit = false;
 }
 
@@ -746,7 +746,7 @@ float BoundedNeuron ::backward(float post_activation) {
     std::cout << "neuron.cpp: invalid backward activation" << std::endl;
     exit(1);
   }
-  if (post_activation = 1)
+  if (post_activation == 1)
     return 1;
   else
     return 0;
