eb086bd326367bfd1ce7ed91044e26efeec3c99a
Khurramjaved <kjaved@ualberta.ca>
2021-09-08 11:23:06 -0600

    Refactor

--- src/utils.cpp
+++ src/utils.cpp
@@ -8,69 +8,50 @@
 #include <iomanip>
 #include "../include/utils.h"
 
-void print_vector(std::vector < float >
-const &v) {
-std::cout << "[";
-int counter = 0;
-for (
-auto i = v.begin();
-i != v.
-end();
-++i){
-std::cout << " " << std::setw(3) << *i << ",";
-if (counter % 50 == 49) std::cout << "\n";
-counter++;
-}
-std::cout << "]\n";
+void print_vector(std::vector < float > const &v) {
+  std::cout << "[";
+  int counter = 0;
+  for (auto i = v.begin(); i != v.end(); ++i){
+    std::cout << " " << std::setw(3) << *i << ",";
+    if (counter % 50 == 49) std::cout << "\n";
+    counter++;
+  }
+  std::cout << "]\n";
 }
 
-void print_vector(std::vector < int >
-const &v) {
-std::cout << "[";
-int counter = 0;
-for (
-auto i = v.begin();
-i != v.
-end();
-++i) {
-std::cout << " " << std::setw(3) << *i << ",";
-if (counter % 100 == 99) std::cout << "\n";
-counter++;
-}
-std::cout << "]\n";
+void print_vector(std::vector < int > const &v) {
+  std::cout << "[";
+  int counter = 0; for (auto i = v.begin(); i != v.end(); ++i) {
+    std::cout << " " << std::setw(3) << *i << ",";
+    if (counter % 100 == 99) std::cout << "\n";
+    counter++;
+  }
+  std::cout << "]\n";
 }
 
-void print_matrix(std::vector < std::vector < int >>
-const &v) {
-int counter = 0;
-for (
-int temp = 0;
-temp<v.
-size();
-temp++) {
-if (temp > 50) {
-std::cout << "Truncating output\n";
-break;
-}
-std::cout << "[";
-int counter_inner = 0;
-for (
-auto i = v[temp].begin();
-i != v[temp].
-end();
-++i) {
-if (counter_inner > 50) {
-std::cout << ", ... , ]";
-break;
-}
-std::cout << " " << std::setw(3) << *i << ",";
-//            if (counter % 50 == 49) std::cout << "\n";
-counter++;
-counter_inner++;
-}
-std::cout << "]\n";
-}
-std::cout << "\n";
+void print_matrix(std::vector < std::vector < int >> const &v) {
+  int counter = 0;
+  for (
+  int temp = 0; temp<v.size(); temp++) {
+    if (temp > 50) {
+      std::cout << "Truncating output\n";
+      break;
+    }
+    std::cout << "[";
+    int counter_inner = 0;
+    for ( auto i = v[temp].begin(); i != v[temp].end(); ++i) {
+      if (counter_inner > 50) {
+        std::cout << ", ... , ]";
+        break;
+      }
+      std::cout << " " << std::setw(3) << *i << ",";
+      //            if (counter % 50 == 49) std::cout << "\n";
+      counter++;
+      counter_inner++;
+      }
+    std::cout << "]\n";
+    }
+  std::cout << "\n";
 }
 
 NetworkVisualizer::NetworkVisualizer(std::vector<Neuron *> all_neurons) {
