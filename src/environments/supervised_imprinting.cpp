4e48279f8db90970f85c65a60ba48f0860883ba4
Khurramjaved <kjaved@ualberta.ca>
2021-12-20 19:13:45 +0500

    Environment for supervised imprinting

--- src/environments/supervised_imprinting.cpp
+++ src/environments/supervised_imprinting.cpp
@@ -15,6 +15,7 @@
 int DISTRIBUTION_SIZE = 30;
 int INPUT_FEATURES = 10;
 float PROB_FLIP = 0.001;
+int TOTAL_FEATURES = 300;
 
 SupervisedImprintingEnv::SupervisedImprintingEnv(int seed) : mt(seed), index_sampler(0, DISTRIBUTION_SIZE - 1) {
   this->input_features = INPUT_FEATURES;
@@ -67,6 +68,7 @@ int SupervisedImprintingEnv::get_index() {
 void SupervisedImprintingEnv::step() {
   this->current_index = this->index_sampler(this->mt);
   std::uniform_real_distribution<float> prob(0, 1);
+//  std::cout << prob(this->mt) << std::endl;
   if(prob(this->mt) < PROB_FLIP)
   {
     this->replace_random_pattern();
@@ -77,7 +79,38 @@ void SupervisedImprintingEnv::step() {
 }
 
 std::vector<float> SupervisedImprintingEnv::get_x() {
-  return this->features[this->current_index];
+  auto features = this->features[this->current_index];
+//  std::vector<float> features_copy;
+
+//  std::cout << "Before\n";
+//  print_vector(features);
+  std::uniform_real_distribution<float> prob(0, 1);
+  for(int i = 0; i< features.size(); i++){
+//    features_copy.push_back(features[i]);
+    if(features[i] == 1){
+        if(prob(this->mt) < 2.0/float(INPUT_FEATURES)){
+//          std::cout << "Flipping 1 to 0\n";
+          features[i] = 0;
+        }
+    }
+    if(features[i] == 0){
+
+      if(prob(this->mt) <  (2.0/float(TOTAL_FEATURES - INPUT_FEATURES))){
+//        std::cout << "Flipping 0 to 1\n";
+        features[i] = 1;
+      }
+    }
+  }
+//  int counter=0;
+//  for(int i = 0; i < features.size(); i++){
+//    if(features[i] != features_copy[i])
+//      counter++;
+//  }
+//  std::cout << "Total flips = " << counter << std::endl;
+//  std::cout << "After\n";
+//  print_vector(features);
+//  std::cout << "\n";
+  return features;
 }
 
 std::vector<float> SupervisedImprintingEnv::get_y() {
@@ -89,12 +122,12 @@ std::vector<float> SupervisedImprintingEnv::get_y() {
 }
 
 std::vector<float> SupervisedImprintingEnv::create_pattern() {
-  std::uniform_int_distribution<int> temp_sampler(0, 29);
+  std::uniform_int_distribution<int> temp_sampler(0, TOTAL_FEATURES-1);
   std::vector<float> temp_state;
-  for (int i = 0; i < 30; i++)
+  for (int i = 0; i < TOTAL_FEATURES; i++)
     temp_state.push_back(0);
 
-  while (sum(temp_state) != 10) {
+  while (sum(temp_state) != INPUT_FEATURES) {
     int s = temp_sampler(this->mt);
     temp_state[s] = 1;
   }
